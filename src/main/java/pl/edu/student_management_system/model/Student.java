05a4b8a6da8db69266a9dcd937b091f4d29a81d6
kkazmier <dev.karol.kazmierczak@gmail.com>
2024-12-26 15:06:23 +0100

    Remove lombok annotations.

--- src/main/java/pl/edu/student_management_system/model/Student.java
+++ src/main/java/pl/edu/student_management_system/model/Student.java
@@ -1,19 +1,48 @@
 package pl.edu.student_management_system.model;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
 
 @Entity
-@Table(name = "drivers", schema = "public")
-@Getter
-@Setter
-public class Student extends BaseEntity {
+@Table(name = "students")
+public class Student {
+
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private int age;
     private double grade;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
 }
